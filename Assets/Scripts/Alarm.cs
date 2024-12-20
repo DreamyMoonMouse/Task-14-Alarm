f6d019ed5d526f10348873101d6b3e3c23c6d11c
DMouse <noname666666666@ya.ru>
2024-12-20 10:57:28 +0300

    fix: refactor

--- Assets/Scripts/Alarm.cs
+++ Assets/Scripts/Alarm.cs
@@ -1,16 +1,20 @@
 using UnityEngine;
+using System.Collections;
 
 public class Alarm : MonoBehaviour
 {
-    [SerializeField] private AudioSource alarmSound;
-    [SerializeField] private float volumeChangeSpeed = 0.5f;
-    private float targetVolume = 0f;
+    [SerializeField] private AudioSource _alarmSound;
+    [SerializeField] private float _volumeChangeSpeed = 0.3f;
+
+    private float _targetVolume = 0f;
+    private Coroutine _volumeCoroutine;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.TryGetComponent(out Thief thief))
         {
-            targetVolume = 1f;
+            _targetVolume = 1f;
+            StartVolumeChange();
         }
     }
 
@@ -18,12 +22,27 @@ public class Alarm : MonoBehaviour
     {
         if (other.TryGetComponent(out Thief thief))
         {
-            targetVolume = 0f; 
+            _targetVolume = 0f;
+            StartVolumeChange();
         }
     }
 
-    private void Update()
+    private void StartVolumeChange()
     {
-        alarmSound.volume = Mathf.MoveTowards(alarmSound.volume, targetVolume, volumeChangeSpeed * Time.deltaTime);
+        if (_volumeCoroutine != null)
+        {
+            StopCoroutine(_volumeCoroutine);
+        }
+        
+        _volumeCoroutine = StartCoroutine(ChangeVolume());
+    }
+
+    private IEnumerator ChangeVolume()
+    {
+        while (Mathf.Abs(_alarmSound.volume - _targetVolume) > Mathf.Epsilon)
+        {
+            _alarmSound.volume = Mathf.MoveTowards(_alarmSound.volume, _targetVolume, _volumeChangeSpeed * Time.deltaTime);
+            yield return null;
+        }
     }
 }
--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,12 +1,14 @@
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Mover : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField] private string _horizontalInputAxis = "Horizontal";
 
     private void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector2.right * horizontalInput * moveSpeed * Time.deltaTime);
+        float horizontalInput = Input.GetAxis(_horizontalInputAxis);
+        transform.Translate(Vector2.right * horizontalInput * _moveSpeed * Time.deltaTime);
     }
 }
